cd57a698483ac66fe2f0d64e6810dde72535533c
Bob Leers <b.leers@open2bizz.nl>
2016-11-03 12:41:07 +0100

    [t#2910] Improvments on WSGIServer-object isolation and support to load Werkzeug it's WSGIServer process-types/models (Singe-threaded, Multi-threaed, Forking) by configuration in the orbeon.server model-record (field: persistence_server_processtype).

--- models/orbeon_server.py
+++ models/orbeon_server.py
@@ -21,8 +21,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-from werkzeug.serving import BaseWSGIServer, ThreadedWSGIServer
-
 from .. import services
 
 import threading
@@ -32,13 +30,20 @@ _logger = logging.getLogger(__name__)
 ORBEON_PERSISTENCE_SERVER_PREFIX = 'orbeon.persistence.server'
 ORBEON_PERSISTENCE_SERVER_INTERFACE = '127.0.0.1'
 
+PERSISTENCE_SERVER_SINGLE_THREADED = 'SINGLE_THREADED'
+PERSISTENCE_SERVER_MULTI_THREADED = 'MULTI_THREADED'
+PERSISTENCE_SERVER_FORKING = 'FORKING'
+
 class OrbeonThreadedWSGIServer(threading.Thread):
     
     def __init__(self, name, server, stopper):
         super(OrbeonThreadedWSGIServer, self).__init__(name=name)
 
-        self.server = server
-        self.stopper = stopper
+        self.local = threading.local()
+        self.local.server = server
+        self.local.stopper = stopper
+        self.server = self.local.server
+        self.stopper = self.local.stopper
 
     def run(self):
         while not self.stopper.is_set():
@@ -69,8 +74,18 @@ class OrbeonServer(models.Model):
     )
 
     persistence_server_port = fields.Char(
-        "Persistence port"
+        "Persistence server port"
     )
+
+    persistence_server_processtype = fields.Selection(
+        [
+            (PERSISTENCE_SERVER_SINGLE_THREADED, "Single thread"),
+            (PERSISTENCE_SERVER_MULTI_THREADED, "Multi thread"),
+            (PERSISTENCE_SERVER_FORKING, "Forking (process)"),
+        ],
+        "Persistence server proces-type",
+        default=PERSISTENCE_SERVER_SINGLE_THREADED,
+        required=True)
 
     # todo Still needed (is_active)?
     is_active = fields.Boolean(
@@ -85,14 +100,11 @@ class OrbeonServer(models.Model):
 
     def __init__(self, pool, cr):
         res = super(OrbeonServer, self).__init__(pool, cr)
-        self._autostart_persistence_servers(pool, cr)
+
+        # TODO enable again
+        #self._autostart_persistence_servers(pool, cr)
         return res
 
-    def _autostart_persistence_servers(self, pool, cr):
-        cr.execute("SELECT persistence_server_port FROM orbeon_server")
-        for (port,) in cr.fetchall():
-            self.start_persistence_server(port)
-        
     @api.one
     @api.depends("url")
     def _set_summary_url(self):
@@ -116,37 +128,67 @@ class OrbeonServer(models.Model):
 
     @api.multi
     def action_start_persistence_server(self, context=None, *args, **kwargs):
-        self.start_persistence_server(self.persistence_server_port)
+        self._start_persistence_server(self.persistence_server_port, self.persistence_server_processtype)
 
     @api.multi
     def action_stop_persistence_server(self, context=None, *args, **kwargs):
-        self.stop_persistence_server(self.persistence_server_port)
+        self._stop_persistence_server(self.persistence_server_port)
 
     # def application(self, environment, start_response):
     #     start_response('200 OK', [('Content-Type', 'text/plain')])
     #     return ['Hello World!']
 
-    def start_persistence_server(self, port):
-        #app = self.application
-        app = services.wsgi_server.app
+    def _persistence_server_name(self, uuid):
+        #TODO
+        # uuid_generate_v4() on start/stop!!! Not here
         
-        server = BaseWSGIServer(ORBEON_PERSISTENCE_SERVER_INTERFACE, port, app)
+        dbuuid = self.env['ir.config_parameter'].get_param('database.uuid')
+        return "%s.%s" % (ORBEON_PERSISTENCE_SERVER_PREFIX, uuid)
+
+    def _persistence_wsgi_server(self, server_processtype):
+        from werkzeug.serving import BaseWSGIServer, ThreadedWSGIServer, ForkingWSGIServer
+
+        if server_processtype == PERSISTENCE_SERVER_SINGLE_THREADED:
+            return BaseWSGIServer
+        elif server_processtype == PERSISTENCE_SERVER_MULTI_THREADED:
+            return ThreadedWSGIServer
+        elif server_processtype == PERSISTENCE_SERVER_FORKING:
+            return ForkingWSGIServer
+
+    def _autostart_persistence_servers(self, pool, cr):
+        try:
+            cr.execute("SELECT persistence_server_port, persistence_server_processtype FROM orbeon_server")
+            for (port, server_processtype) in cr.fetchall():
+                self._start_persistence_server(port, server_processtype)
+        except:
+            pass
+        
+    def _start_persistence_server(self, port, server_processtype):
+        dbuuid = self.env['ir.config_parameter'].get_param('database.uuid')
+
+        app = services.wsgi_server.app
+        wsgi_server = self._persistence_wsgi_server(server_processtype)
+        wsgi_app_server = wsgi_server(ORBEON_PERSISTENCE_SERVER_INTERFACE, port, app)
+        
         stopper = threading.Event()
 
         t = OrbeonThreadedWSGIServer(
-            name=ORBEON_PERSISTENCE_SERVER_PREFIX,
-            server=server,
+            name=self._persistence_server_name(dbuuid),
+            server=wsgi_app_server,
             stopper=stopper
         )
         
         t.setDaemon(True)
-        _logger.info('Initiating HTTP %s (werkzeug) START on port %s', ORBEON_PERSISTENCE_SERVER_PREFIX, port)
+        _logger.info('Initiating HTTP %s (werkzeug) START on port %s', self._persistence_server_name(dbuuid), port)
         t.start()
         
-    def stop_persistence_server(self, port):
+    def _stop_persistence_server(self, port):
+        # TODO uuid_generate and cache/store somehwere?
+        dbuuid = self.env['ir.config_parameter'].get_param('database.uuid')
+        
         for thread in threading.enumerate():
-            if thread.getName() == ORBEON_PERSISTENCE_SERVER_PREFIX:
+            if thread.getName() == self.persistence_server_name(dbuuid):
                 thread.stopper.set()
-                _logger.info("Initiating HTTP %s (werkzeug) SHUTDOWN on port %s", ORBEON_PERSISTENCE_SERVER_PREFIX, port)
+                _logger.info("Initiating HTTP %s (werkzeug) SHUTDOWN on port %s", self.persistence_server_name(dbuuid), port)
                 thread.server.server_close()
                 thread.join()
