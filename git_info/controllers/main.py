b7706113f7dab4553aceb5711bbb002d62a1e316
Bob Leers <b.leers@open2bizz.nl>
2017-05-01 14:58:15 +0200

    git_info: Fix git log command. Append current dir which is cwd.

--- git_info/controllers/main.py
+++ git_info/controllers/main.py
@@ -49,7 +49,7 @@ class GitInfoWebSettingsDashboard(WebSettingsDashboard):
 
         repo_dir = config['root_path']
         git_log = subprocess.Popen(
-            'git log --pretty=format:%ct --quiet -1 HEAD',
+            'git log --pretty=format:%ct --quiet -1 HEAD .',
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             shell=True, cwd=repo_dir, universal_newlines=True,
         )
