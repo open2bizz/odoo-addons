342597720dccd7e3ebccbb4093666174977c55c0
Thomas Pot <t.pot@open2bizz.nl>
2020-10-03 18:47:28 +0200

    Add new module default data on notes

---
+++ default_data task_notes/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright Open2bizz (http://www.open2bizz.nl)
+# See LICENSE file for full copyright and license details.
+{
+    'name': 'Default Data for notes',
+    'version': '0.1',
+    'category': 'Tools',
+    'description': """default data for notes in Odoo.""",
+    'author': 'Open2bizz',
+    'website': 'http://open2bizz.nl/',
+    'license': "LGPL-3",
+    'depends': ['note','project_task_notes'],
+    'data':[
+        'views/view_notes.xml',
+    ],
+    'installable': True,
+}
---
+++ default_data task_notes/models/note_note.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    open2bizz
+#    Copyright (C) 2020 open2bizz (open2bizz.nl).
+#    See licence file
+##############################################################################
+
+from odoo import api, fields, models, _
+from odoo.exceptions import  ValidationError
+import logging
+
+class Note(models.Model):
+
+    _inherit = 'note.note'
+    
+    solution = fields.Html(
+        string = 'Solution'
+    )
+    default_data_id = fields.Many2one(
+        comodel_name = 'default.data',
+        string = 'Default Solution',
+        domain = "[('model', '=', 'note.note'), ('field', '=', 'memo')]"
+    )
+    
+    @api.onchange("default_data_id")
+    def onchange_default_data_id(self):
+        if self.default_data_id:
+            update_data = self.default_data_id.get_update_default_data(self.solution)
+            if update_data['update']:
+                self.default_data_id = update_data['data']
+
+    def action_create_default_data(self):
+        value = self.memo
+        name_new = self.memo[:10]
+        model_id = self.env['ir.model'].search([('model','=','note.note')])
+        field_id = self.env['ir.model.fields'].search([('model','=','note.note'),('name','=','memo')])
+        default_data = self.env['default.data'].create({'model_id': model_id.id,'name': name_new ,'field_id': field_id.id ,'value_html': self.memo, 'type': 'html'})
+        return {
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form,tree',
+                'res_model': 'default.data',
+                'target': 'current',
+                'res_id': default_data.id,
+                'domain': [('model','=','note.note')],
+            } 
